123288025809e6a1dce39a2bcd551bf2e786bd23
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2024-04-20 23:16:25 +0200

    first commit: Load initial files

---
+++ Custom Calendar/Custom_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_CalendarApp.swift
+//  Custom Calendar
+//
+//  Created by Marcin Jędrzejak on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
